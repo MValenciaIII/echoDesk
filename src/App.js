6632df1e1fbaa31df36e31da0249873bdaa8f0fd
Will Kelly <willkelly9617@gmail.com>
2021-02-08 18:32:54 -0600

    Commit to test my home computer's connection to git

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@
 
 // export default App;
 
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './components/Home';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
