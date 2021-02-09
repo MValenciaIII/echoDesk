4e59c643fa82b978d28607e069a4c7bb25eced0c
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2021-02-09 08:31:15 -0600

    Footer
    Created a couple CSS style tags for the footer and created the footer as well.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,39 @@ import { Link } from "react-router-dom";
 function Footer(props) {
   return (
     <>
-      <h2>Footer</h2>
+    <div class="footer-background bg-blue ">
+        <div class="container ">
+          <div className="grid grid-cols-6   ">
+              <div className="col-start-2 col-span-1 text-white ">
+                <p class="footer-logo text-2xl font-semibold">EchoDesk</p>
+              </div>
+              <div className="text-white">
+                <p class="footer-ulTitle text-xl font-semibold ">Get Started</p>
+                <ul>
+                  <li class="footer-li text-xs">Dashboard</li>
+                  <li class="footer-li text-xs">Create a ticket</li>
+                  <li class="footer-li text-xs">Login</li>
+                </ul>
+              </div>
+              <div className="text-white">
+              <p class="text-xl font-semibold footer-ulTitle">About us</p>
+                <ul>
+                  <li class="footer-li text-xs">Contact us</li>
+                  <li class="footer-li text-xs">About us</li>
+                  <li class="footer-li text-xs">lorem epson</li>
+                </ul>
+              </div>
+              <div className="text-white">
+              <p class="text-xl font-semibold footer-ulTitle">Support</p>
+                <ul>
+                  <li class="footer-li text-xs">FAQ</li>
+                  <li class="footer-li text-xs">HelpDesk</li>
+                  <li class="footer-li text-xs">Forms</li>
+                </ul>
+              </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
