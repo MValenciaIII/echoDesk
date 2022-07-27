f89fb8713f408ff3a70caa717a185cccc8dd732e
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2022-07-27 15:22:51 -0500

    Update dbUserContext.js

--- src/context/dbUserContext.js
+++ src/context/dbUserContext.js
@@ -67,12 +67,6 @@ function UserContextProvider(props) {
     setThemeColor(newTheme);
   }
 
-
-
-
-
-
-
   function fetchFilterPreference() {
     if(localStorage.setFilterPreference === 'true') {
       console.log('setting true')
@@ -97,13 +91,6 @@ function UserContextProvider(props) {
     }
 
   }
-
-
-
-
-
-
-
 
   // Again, should probably make this reference the utility function that is defined in util for it;   ~wk 5/4
   let barIndex;
