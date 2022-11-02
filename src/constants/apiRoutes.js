930378d72f318ed20c147b747ab8615fdc954d50
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2022-11-02 12:48:27 -0500

    Change test server ip

--- src/constants/apiRoutes.js
+++ src/constants/apiRoutes.js
@@ -1,16 +1,16 @@
 //ALL API ROUTES ARE DEFINED HERE AS THE SOURCE OF TRUTH FOR API
 
-const baseApiUrl = 'https://www.mema4kids.info/api/';
+const baseApiUrl = 'http://10.250.138.41:3075/api/';
 
 
-export const allTicketsRoute = 'https://www.mema4kids.info/api/tickets';
+export const allTicketsRoute = 'http://10.250.138.41:3075/api/tickets';
 
 
 export function departmentTicketsRoute (id) {
   return baseApiUrl.concat(`tickets/departmenttickets/${id}`);
 }
 
-// export const departmentTicketsRoute = 'https://www.mema4kids.info/api/tickets/departmenttickets/2'
+// export const departmentTicketsRoute = 'http://10.250.138.41:3075/api/tickets/departmenttickets/2'
 
 export function dbUserRoute(id) {
   return baseApiUrl.concat(`users/${id}`);
@@ -19,7 +19,7 @@ export function dbUsersTicketsRoute(id) {
   return baseApiUrl.concat(`tickets/${id}`);
 }
 export const createTicketRoute =
-  'https://www.mema4kids.info/api/tickets/create';
+  'http://10.250.138.41:3075/api/tickets/create';
 
 export const filteringRoute = baseApiUrl.concat('tickets/filter/search?');
 
